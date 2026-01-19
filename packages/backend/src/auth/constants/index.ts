fc0a92ff60182c121071eb149444f19c8c540ce1
Taras Huledza <Taras Huledza>
2026-01-19 22:50:34 +0200

    Revert "first commit"
    This reverts commit b007ab2b7b30ae90cf6cf8a0aff009864673bae5.

--- packages/backend/src/auth/constants/index.ts
+++
@@ -1,5 +0,0 @@
-
-export const jwtConstants = {
-  secret: process.env.JWT_SECRET,
-  expiresIn: parseInt(process.env.JWT_EXPIRES_IN),
-};
--- packages/shared/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { prisma } from './prisma/client'
-export * from './prisma/types'
-export * from './types/languages'
-
