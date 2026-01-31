bb6e1c4be148a67b47e2ddc0b084e1bf2384e38a
Taras Huledza <Taras Huledza>
2026-01-31 20:33:23 +0200

    fix: update default role in Auth decorator to MECHANIC

--- packages/backend/src/auth/decorators/auth.decorator.ts
+++ packages/backend/src/auth/decorators/auth.decorator.ts
@@ -1,11 +1,11 @@
-import { UseGuards, applyDecorators } from '@nestjs/common'
-import { AuthGuard } from '@nestjs/passport'
-import { Role } from 'prisma/generated/prisma/enums'
-import { RolesGuard } from '../roles/roles.guard'
+import { UseGuards, applyDecorators } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Role } from 'prisma/generated/prisma/enums';
+import { RolesGuard } from '../roles/roles.guard';
 
-export const Auth = (role: Role = Role.CLIENT) =>
+export const Auth = (role: Role = Role.MECHANIC) =>
 	applyDecorators(
 		role === Role.ADMIN
-			? UseGuards(AuthGuard('jwt'), RolesGuard) 
-			: UseGuards(AuthGuard('jwt'))
+			? UseGuards(AuthGuard('jwt'), RolesGuard)
+			: UseGuards(AuthGuard('jwt')),
 	);
